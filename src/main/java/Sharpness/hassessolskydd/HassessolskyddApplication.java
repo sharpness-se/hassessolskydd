f74ebec4bea8bd16356c9af49576d9ba6c20f04b
Miguel <miguel.h.ogren@gmail.com>
2023-10-24 14:20:10 +0200

    First commit

---
+++ src/main/java/Sharpness/hassessolskydd/HassessolskyddApplication.java
@@ -0,0 +1,13 @@
+package Sharpness.hassessolskydd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HassessolskyddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HassessolskyddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sharpness/hassessolskydd/HassessolskyddApplicationTests.java
@@ -0,0 +1,13 @@
+package Sharpness.hassessolskydd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HassessolskyddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
