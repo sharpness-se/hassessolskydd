1a7d871eba898d7fe9bba1bda93aca4e45cacc46
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-11-27 14:43:04 +0100

    removed empty test

--- server/src/test/java/se/sharpness/hassessolskydd/HassessolskyddApplicationTests.java
+++ server/src/test/java/se/sharpness/hassessolskydd/HassessolskyddApplicationTests.java
@@ -1,13 +1,13 @@
-package se.sharpness.hassessolskydd;
+// package se.sharpness.hassessolskydd;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HassessolskyddApplicationTests {
+// @SpringBootTest
+// class HassessolskyddApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	// @Test
+// 	// void contextLoads() {
+// 	// }
 
-}
+// }
