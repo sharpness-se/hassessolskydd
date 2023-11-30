5ec866cce6520a802901b0b96ba4d04d05841122
JohanssonJacob <jacobjohanssn2001@gmail.com>
2023-11-30 16:21:16 +0100

    Validation for customer

--- server/src/main/java/se/sharpness/hassessolskydd/model/Customer.java
+++ server/src/main/java/se/sharpness/hassessolskydd/model/Customer.java
@@ -1,5 +1,6 @@
 package se.sharpness.hassessolskydd.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,12 +9,33 @@ import lombok.NoArgsConstructor;
 public class Customer {
 
     private int id;
+
+    @NotBlank
+    @Size(max = 20)
     private String firstname;
+
+    @NotBlank
+    @Size(max = 20)
     private String lastname;
+
+    @NotBlank
+    @Size(max = 20)
     private String address;
+
+    @NotBlank
+    @Size(max = 5)
     private String postalCode;
+
+    @NotBlank
+    @Size(max = 20)
     private String city;
+
+    @NotBlank
+    @Size(max = 12)
     private String phoneNumber;
+
+    @NotBlank
+    @Email
     private String email;
     private String customerNumber;
 }
