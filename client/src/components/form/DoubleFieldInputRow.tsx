dcb754115df469760768aae082512d09c9d7c73c
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-11-30 11:25:49 +0100

    added Validation to custom form component

--- client/src/components/form/DoubleFieldInputRow.tsx
+++ client/src/components/form/DoubleFieldInputRow.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 interface DoubleFieldInputRowProps {
   labelOne: string;
   labelTwo: string;
-  placeholderOne: string;
-  placeholderTwo: string;
+  placeholderOne?: string;
+  placeholderTwo?: string;
+  children?: ReactNode;
+  onChangeOne: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeTwo: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  errorOne: string;
+  errorTwo: string;
+  valueOne: string;
+  valueTwo: string;
+  maxLengthOne: number;
+  maxLengthTwo: number;
 }
 const DoubleFieldInputRow: React.FC<DoubleFieldInputRowProps> = ({
   labelOne,
   labelTwo,
   placeholderOne,
   placeholderTwo,
+  valueOne,
+  valueTwo,
+  onChangeOne,
+  onChangeTwo,
+  errorOne,
+  errorTwo,
+  maxLengthOne,
+  maxLengthTwo,
+
 }) => {
   return (
     <div className="flex flex-wrap -mx-3 mb-6">
@@ -22,14 +40,16 @@ const DoubleFieldInputRow: React.FC<DoubleFieldInputRowProps> = ({
           {labelOne}
         </label>
         <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${valueOne.length===0?"border-red-500":""} rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
           id={labelOne}
+          autoComplete={labelOne}
           type="text"
           placeholder={placeholderOne}
+          value={valueOne}
+          onChange={onChangeOne}
+          maxLength={maxLengthOne}
         />
-        <p className="text-red-500 text-xs italic">
-          Please fill out this field.
-        </p>
+        {errorOne && <p className="text-red-500 text-xs italic">{errorOne} </p>}
       </div>
       <div className="w-full md:w-1/2 px-3">
         <label
@@ -39,14 +59,15 @@ const DoubleFieldInputRow: React.FC<DoubleFieldInputRowProps> = ({
           {labelTwo}
         </label>
         <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${valueTwo.length===0?"border-red-500":""} rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
           id={labelTwo}
           type="text"
           placeholder={placeholderTwo}
+          value={valueTwo}
+          onChange={onChangeTwo}
+          maxLength={maxLengthTwo}
         />
-        <p className="text-red-500 text-xs italic">
-          Please fill out this field.
-        </p>
+        {errorTwo && <p className="text-red-500 text-xs italic">{errorTwo} </p>}
       </div>
     </div>
   );
