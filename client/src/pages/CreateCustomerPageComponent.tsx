49b002861694a15d7c940fa7311f872bbc3d46d4
arapg <hello@arpg.dev>
2023-12-05 14:34:39 +0100

    temporary title fix

--- client/src/pages/CreateCustomerPageComponent.tsx
+++ client/src/pages/CreateCustomerPageComponent.tsx
@@ -5,8 +5,8 @@ export default function CreateCustomerPageComponent() {
   
   return (
     <div className="flex min-h-screen flex-col items-center p-24 ">
-      <h1 className="text-5xl bg-cyan-500 text-black border-black border-4 p-10 mb-11">
-        Create Customer
+      <h1 className="text-5xl mb-11">
+        Skapa ny kund
       </h1>
       <CreateCustomerComponent/>
     </div>
