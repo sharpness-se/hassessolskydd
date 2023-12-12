3c178efc5902710d1bf664dc03d6b44a652981bf
JohanssonJacob <jacobjohanssn2001@gmail.com>
2023-12-12 11:08:19 +0100

    Updated CustomerTest

--- server/src/test/java/se/sharpness/hassessolskydd/integrationstest/customer/CustomerTest.java
+++ server/src/test/java/se/sharpness/hassessolskydd/integrationstest/customer/CustomerTest.java
@@ -5,6 +5,8 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import se.sharpness.hassessolskydd.model.Customer;
 
 import java.util.Set;
@@ -18,93 +20,58 @@ class CustomerTest {
 
   @Test
   void validCustomer() {
-    Customer customer = new Customer();
-    customer.setFirstname("John");
-    customer.setLastname("Doe");
-    customer.setAddress("123 Main St");
-    customer.setPostalCode("12345");
-    customer.setCity("Cityville");
-    customer.setPhoneNumber("1234567890");
-    customer.setEmail("john.doe@example.com");
-    customer.setCustomerNumber("C12345");
-
+    Customer customer = createValidCustomer();
     Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
-
     assertTrue(violations.isEmpty(), "Validation should pass");
   }
 
-  @Test
-  void validFirstname() {
-    Customer customer = new Customer();
-    customer.setFirstname("John");
-
-    customer.setLastname("Doe");
-    customer.setAddress("123 Main St");
-    customer.setPostalCode("12345");
-    customer.setCity("Cityville");
-    customer.setPhoneNumber("1234567890");
-    customer.setEmail("john.doe@example.com");
-    customer.setCustomerNumber("C12345");
-
+  @ParameterizedTest
+  @ValueSource(strings = {"John", "Alice", "Bob"})
+  void validFirstname(String validFirstname) {
+    Customer customer = createValidCustomer();
+    customer.setFirstname(validFirstname);
     Set<ConstraintViolation<Customer>> violations = validator.validateProperty(customer, "firstname");
-
-    assertTrue(violations.isEmpty(), "Validation should pass for valid firstname");
+    assertTrue(violations.isEmpty(), "Validation should pass for valid firstname: " + validFirstname);
   }
 
-  @Test
-  void invalidFirstname() {
-    Customer customer = new Customer();
-
-    customer.setFirstname("John123");
-
-    customer.setLastname("Doe");
-    customer.setAddress("123 Main St");
-    customer.setPostalCode("12345");
-    customer.setCity("Cityville");
-    customer.setPhoneNumber("1234567890");
-    customer.setEmail("john.doe@example.com");
-    customer.setCustomerNumber("C12345");
-
+  @ParameterizedTest
+  @ValueSource(strings = {"John123", "123John", "John!"})
+  void invalidFirstname(String invalidFirstname) {
+    Customer customer = createValidCustomer();
+    customer.setFirstname(invalidFirstname);
     Set<ConstraintViolation<Customer>> violations = validator.validateProperty(customer, "firstname");
-
-    assertFalse(violations.isEmpty(), "Validation should fail for invalid firstname");
+    assertFalse(violations.isEmpty(), "Validation should fail for invalid firstname: " + invalidFirstname);
   }
 
-  @Test
-  void validLastname() {
+  @ParameterizedTest
+  @ValueSource(strings = {"Doe", "Smith", "Johnson"})
+  void validLastname(String validLastname) {
+    Customer customer = createValidCustomer();
+    customer.setLastname(validLastname);
+    Set<ConstraintViolation<Customer>> violations = validator.validateProperty(customer, "lastname");
+    assertTrue(violations.isEmpty(), "Validation should pass for valid lastname: " + validLastname);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"Doe123", "123Doe", "Doe!"})
+  void invalidLastname(String invalidLastname) {
+    Customer customer = createValidCustomer();
+    customer.setLastname(invalidLastname);
+    Set<ConstraintViolation<Customer>> violations = validator.validateProperty(customer, "lastname");
+    assertFalse(violations.isEmpty(), "Validation should fail for invalid lastname: " + invalidLastname);
+  }
+
+  private Customer createValidCustomer() {
     Customer customer = new Customer();
+    customer.setFirstname("John");
     customer.setLastname("Doe");
-
-    customer.setFirstname("John");
     customer.setAddress("123 Main St");
     customer.setPostalCode("12345");
     customer.setCity("Cityville");
     customer.setPhoneNumber("1234567890");
     customer.setEmail("john.doe@example.com");
     customer.setCustomerNumber("C12345");
-
-    Set<ConstraintViolation<Customer>> violations = validator.validateProperty(customer, "lastname");
-
-    assertTrue(violations.isEmpty(), "Validation should pass for valid lastname");
-  }
-
-  @Test
-  void invalidLastname() {
-    Customer customer = new Customer();
-
-    customer.setLastname("Doe123");
-
-    customer.setFirstname("John");
-    customer.setAddress("123 Main St");
-    customer.setPostalCode("12345");
-    customer.setCity("Cityville");
-    customer.setPhoneNumber("1234567890");
-    customer.setEmail("john.doe@example.com");
-    customer.setCustomerNumber("C12345");
-
-    Set<ConstraintViolation<Customer>> violations = validator.validateProperty(customer, "lastname");
-
-    assertFalse(violations.isEmpty(), "Validation should fail for invalid lastname");
+    return customer;
   }
 
 
