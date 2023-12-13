b233bd8583adb115004ad5b0fb9f34e0d8c5542b
arapg <hello@arpg.dev>
2023-12-13 22:17:07 +0100

    changed back button styling

--- client/src/components/form/BackButton.tsx
+++ client/src/components/form/BackButton.tsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 
 const BackButton = () => {
     return (
-        <Link to={'/'} className="mr-4">Tillbaka</Link>
+        <Link to={'/'} className="mr-4">
+            <button className="rounded-full bg-gray-100 hover:bg-gray-50 shadow-md text-xl font-bold px-12 py-3">
+                Tillbaka
+            </button>
+        </Link>
     );
   };
   
