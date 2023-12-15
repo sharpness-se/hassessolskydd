d4ebc87fe34a233d91707859bffce18fbb5d0c7e
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-12-15 19:15:12 +0100

    testing url fix

--- client/src/settings/baseUrl.tsx
+++ client/src/settings/baseUrl.tsx
@@ -1,1 +1,2 @@
-export const baseUrl = process.env.NODE_ENV === "production"? process.env.REACT_APP_BACKEND_URL : "http://localhost:8080"
+export const baseUrl ="https://hasses-be-c8520bea6cc2.herokuapp.com"
+    //process.env.NODE_ENV === "production" ? process.env.REACT_APP_BACKEND_URL : "http://localhost:8080"
