f9967d731c353af49773d6555a8f2188e2a7da96
arapg <hello@arpg.dev>
2023-12-20 11:42:36 +0100

    removed margin from first title in customer info box

--- client/src/components/CustomerDetailsComponent.tsx
+++ client/src/components/CustomerDetailsComponent.tsx
@@ -10,7 +10,7 @@ const CustomerDetailsComponent: React.FC<CustomerDetailsProps> = ({
 }) => {
   return (
     <div className="bg-white rounded p-5 flex-grow h-auto ml-5 w-64">
-      <p className="uppercase tracking-wide text-gray-700 text-xs font-bold mt-5">
+      <p className="uppercase tracking-wide text-gray-700 text-xs font-bold">
         Kundnummer
       </p>
       <p id="kundnummer">{customer ? customer.customerNumber : "-"}</p>
