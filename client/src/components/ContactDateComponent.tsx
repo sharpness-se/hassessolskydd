c0b0d131789831b0bfe423583b257c601f87bafd
arapg <hello@arpg.dev>
2023-12-21 11:39:29 +0100

    added current date to order page

--- client/src/components/ContactDateComponent.tsx
+++ client/src/components/ContactDateComponent.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
 interface ContactDateProps{
     heading: string,
 }
 
 const ContactDateComponent: React.FC<ContactDateProps> =({heading})=> {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date());
+
   return (
-    <div className="bg-white rounded p-5 w-full">
-            <h2 className="uppercase tracking-wide text-gray-700 text-s font-bold">
-              {heading}
-            </h2>
-          </div>
+    <div className="bg-white rounded p-5 w-full ">
+      <h2 className="uppercase tracking-wide text-gray-700 text-s font-bold mb-3">
+        {heading}
+      </h2>
+      <p className="uppercase tracking-wide text-gray-700 text-xs font-bold">
+        Kontaktdatum
+      </p>
+      <p>
+        {currentDate.toLocaleDateString()}  
+      </p>
+    </div>
   )
 }
 
