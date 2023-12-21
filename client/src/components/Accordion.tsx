6ac5db732440d567363f5e87ab828bf9a9d6f958
arapg <hello@arpg.dev>
2023-12-21 15:07:36 +0100

    added toggle logic and icons

--- client/src/components/Accordion.tsx
+++ client/src/components/Accordion.tsx
@@ -1,23 +1,34 @@
 import React, { ReactNode, useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
 
 interface AccordionProps {
-    title: string;
-    children: ReactNode;
+  title: string;
+  children: ReactNode;
 }
 
 const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-      <div className="bg-white rounded p-5 w-full">
-          <div>
-              <h2 className="uppercase tracking-wide text-gray-700 text-s font-bold mb-3">
-                  {title}
-               </h2>
-               <button>
-                v
-               </button>
-          </div>  
+    <div className="bg-white rounded p-5 w-full">
+      <div
+        className="flex justify-between items-center cursor-pointer"
+        onClick={() => setIsExpanded((prevIsExpanded) => !prevIsExpanded)}
+      >
+        <h2 className="uppercase tracking-wide text-gray-700 text-s font-bold">
+          {title}
+        </h2>
+        <button>
+          {isExpanded ? (
+            <FaChevronUp className="align-middle" />
+          ) : (
+            <FaChevronDown className="align-middle" />
+          )}
+        </button>
       </div>
-    );
-}
+      {isExpanded && <div className="mt-5">{children}</div>}
+    </div>
+  );
+};
 
 export default Accordion;
