a2ad202d34b3442c57da06e68a6829c53912c168
arapg <hello@arpg.dev>
2023-12-21 15:08:03 +0100

    formatting

--- client/src/components/ContactDateComponent.tsx
+++ client/src/components/ContactDateComponent.tsx
@@ -1,26 +1,23 @@
 import React, { useState } from "react";
 
-interface ContactDateProps{
-    heading: string,
+interface ContactDateProps {
+  heading: string;
 }
 
-const ContactDateComponent: React.FC<ContactDateProps> =({heading})=> {
+const ContactDateComponent: React.FC<ContactDateProps> = ({ heading }) => {
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
 
   return (
-    <div className="bg-white rounded p-5 w-full ">
+    <div className="bg-white rounded p-5 w-full">
       <h2 className="uppercase tracking-wide text-gray-700 text-s font-bold mb-3">
         {heading}
       </h2>
       <p className="uppercase tracking-wide text-gray-700 text-xs font-bold">
         Kontakt
       </p>
-      <p>
-        {currentDate.toLocaleDateString()}  
-      </p>
+      <p>{currentDate.toLocaleDateString()}</p>
     </div>
-  )
-}
+  );
+};
 
-
-export default ContactDateComponent
+export default ContactDateComponent;
