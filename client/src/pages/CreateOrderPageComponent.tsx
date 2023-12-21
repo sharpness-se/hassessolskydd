7ca79a3be879b30cfd32f28d445e10453d1ff0e9
arapg <hello@arpg.dev>
2023-12-21 15:06:30 +0100

    added accordion and fixed flex layout of search and cust info

--- client/src/pages/CreateOrderPageComponent.tsx
+++ client/src/pages/CreateOrderPageComponent.tsx
@@ -4,6 +4,7 @@ import { Customer } from "../components/searchBar/CustomSearch";
 import CustomerDetailsComponent from "../components/CustomerDetailsComponent";
 import ContactDateComponent from "../components/ContactDateComponent";
 import SearchBarComponent from "../components/searchBar/MuiSearchBarComponent";
+import Accordion from "../components/Accordion";
 
 export default function CreateOrderPageComponent() {
   const [customer, setCustomer] = useState<Customer | undefined>(undefined);
@@ -15,13 +16,25 @@ export default function CreateOrderPageComponent() {
   return (
     <div className="flex min-h-screen flex-col items-center p-24 ">
       <h1 className="text-5xl mb-11">Skapa Order</h1>
-      <div className="flex">
-        <div className="flex flex-col flex-grow">
-          <SearchBarComponent onCustomerSelect={handleCustomerSelect}/>
+      <div className="flex mb-5 w-full">
+        <div className="flex flex-col flex-1">
+          <SearchBarComponent onCustomerSelect={handleCustomerSelect} />
           <ContactDateComponent heading={"Datum"} />
         </div>
-        <CustomerDetailsComponent customer={customer} />
+        <div className="flex-2">
+          <CustomerDetailsComponent customer={customer} />
+        </div>
       </div>
+      <Accordion title={"Typ av ärende"}>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            omnis quo. Perferendis, quos temporibus asperiores, fugit veritatis
+            sed repellendus voluptate laudantium illum aspernatur vero?
+            Quibusdam doloribus dolor voluptates natus est.
+          </p>
+        </div>
+      </Accordion>
     </div>
   );
 }
