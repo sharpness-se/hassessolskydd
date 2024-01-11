2997b6f889998141abe3187b713fc36a1b92f0a6
Mikko Tirronen <mikkoatirronen@gmail.com>
2024-01-11 10:00:47 +0100

    minor change to handle undefined link

--- client/src/components/StartMenuButtonComponent.tsx
+++ client/src/components/StartMenuButtonComponent.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 interface StartMenuButtonProps {
   text: string;
-  href: string;
+  href?: string;
 }
 
 const StartMenuButton: React.FC<StartMenuButtonProps> = ({ text, href }) => {
   return (
     <Link
-      to={href}
+      to={href? href: ""}
       className="flex justify-center items-center rounded-full bg-white hover:bg-blue-600 shadow-xl hover:text-white text-2xl w-[331px] h-[70px] m-5"
     >
       {text}
