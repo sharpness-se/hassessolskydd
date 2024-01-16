664ece863f0043c62e22bbafc48a26efc626ff34
Mikko Tirronen <mikkoatirronen@gmail.com>
2024-01-16 14:26:30 +0100

    added props to accordian

--- client/src/components/AccordionComponent.tsx
+++ client/src/components/AccordionComponent.tsx
@@ -3,14 +3,21 @@ import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
 
 interface AccordionProps {
   title: string;
+  addDelete?: boolean;
   children: ReactNode;
+  applyHeight?: boolean;
 }
 
-const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
+const Accordion: React.FC<AccordionProps> = ({
+  title,
+  addDelete,
+  children,
+  applyHeight,
+}) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div className="bg-white rounded p-5 mx-5 max-w-3xl w-[715px]">
+    <div className={`bg-white rounded p-5 max-w-[44.5rem] w-[100%] `}>
       <div
         className="flex justify-between items-center cursor-pointer"
         onClick={() => setIsExpanded((prevIsExpanded) => !prevIsExpanded)}
@@ -18,15 +25,22 @@ const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
         <h2 className="uppercase tracking-wide text-gray-700 text-s font-bold">
           {title}
         </h2>
-        <button>
-          {isExpanded ? (
-            <FaChevronUp className="align-middle" />
-          ) : (
-            <FaChevronDown className="align-middle" />
+        <div>
+          {addDelete && (
+            <button className="bg-white h-min px-2 py-1 hover:bg-red-600 hover:text-white rounded text-xs font-bold mr-5 align-middle">
+              TA BORT
+            </button>
           )}
-        </button>
+          <button className="">
+            {isExpanded ? (
+              <FaChevronUp className="align-middle" />
+            ) : (
+              <FaChevronDown className="align-middle" />
+            )}
+          </button>
+        </div>
       </div>
-      {isExpanded && <div className="mt-5">{children}</div>}
+      {isExpanded && <div className={`mt-5`}>{children}</div>}
     </div>
   );
 };
