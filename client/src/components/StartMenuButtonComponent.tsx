99b3d3c89e9f4c11f3712ee887aaac5ec7b9bb83
Mikko Tirronen <mikkoatirronen@gmail.com>
2024-01-22 13:19:51 +0100

    changed link to button when handlesubmit is passed as prop

--- client/src/components/StartMenuButtonComponent.tsx
+++ client/src/components/StartMenuButtonComponent.tsx
@@ -4,17 +4,33 @@ import { Link } from "react-router-dom";
 interface StartMenuButtonProps {
   text: string;
   href?: string;
+  handleSubmit?: () => Promise<void>;
 }
 
-const StartMenuButton: React.FC<StartMenuButtonProps> = ({ text, href }) => {
-  return (
-    <Link
-      to={href? href: ""}
-      className="flex justify-center items-center rounded-full bg-white hover:bg-blue-600 shadow-xl hover:text-white text-2xl w-[331px] h-[70px] m-5"
-    >
-      {text}
-    </Link>
-  );
+const StartMenuButton: React.FC<StartMenuButtonProps> = ({
+  text,
+  href,
+  handleSubmit,
+}) => {
+  const styling =
+    "flex justify-center items-center rounded-full bg-white hover:bg-blue-600 shadow-xl hover:text-white text-2xl w-[331px] h-[70px] m-5";
+
+  const renderbutton = () => {
+    if (handleSubmit) {
+     
+        return <button className={`${styling}`} onClick={() => handleSubmit()}>
+          {text}
+        </button>
+    
+    } else {
+      return (
+        <Link to={href ? href : ""} className={`${styling}`}>
+          {text}
+        </Link>
+      );
+    }
+  };
+  return renderbutton();
 };
 
 export default StartMenuButton;
