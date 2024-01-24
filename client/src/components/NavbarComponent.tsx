f8ebb7d45477792c9b53b0be206eec6dd3c404fe
arapg <hello@arpg.dev>
2024-01-24 17:55:52 +0100

    created navbar component

---
+++ client/src/components/NavbarComponent.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaX } from "react-icons/fa6";
+
+interface NavbarProps {
+  title: string;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ title }) => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <nav className="bg-blue-500 p-5 text-white fixed top-0 w-full flex justify-between z-50">
+        <Link to={"/"}>
+          <p>Tillbaka</p>
+        </Link>
+        <h2 className="text-xl font-bold">{title}</h2>
+        <button onClick={toggleMenu}>
+          {isMenuOpen ? <FaX /> : <FaBars />}
+        </button>
+      </nav>
+      {isMenuOpen && (
+        <div className="bg-white w-80 h-full p-5 fixed top-16 right-0 z-0"></div>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
