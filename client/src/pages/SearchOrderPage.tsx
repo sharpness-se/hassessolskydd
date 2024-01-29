5eaed987dd4c803e479b4027746ad1345a22b559
arapg <hello@arpg.dev>
2024-01-29 19:15:04 +0100

    moved page title to navbar

--- client/src/pages/SearchOrderPage.tsx
+++ client/src/pages/SearchOrderPage.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import Navbar from "../components/NavbarComponent";
 
 export default function SearchOrderPage() {
- 
   return (
-    <div className="flex min-h-screen flex-col items-center p-20 xl:px">
-      <h1 data-test="search-heading" className="text-5xl mb-11 min-w-max">
+    <>
+      <Navbar title="Sök ordrar" />
+      <div className="flex min-h-screen flex-col items-center p-20 xl:px">
+        {/* <h1 data-test="search-heading" className="text-5xl mb-11 min-w-max">
         Sök Ordrar
-      </h1>
-      <div className="w-full rounded-lg p-10 bg-white shadow-md">
-        <h2 className="text-xl font-bold text-gray-700 mb-3">Sök Ordrar</h2>
-      
+      </h1> */}
+        <div className="w-full rounded-lg p-10 bg-white shadow-md">
+          <h2 className="text-xl font-bold text-gray-700 mb-3">Sök Ordrar</h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
