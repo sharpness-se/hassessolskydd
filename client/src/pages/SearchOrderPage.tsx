79c19b3d584fd8cfc11a9e26550e6ced1238d681
arapg <hello@arpg.dev>
2024-01-31 15:16:52 +0100

    added navbar to Se Ordrar

--- client/src/pages/SearchOrderPage.tsx
+++ client/src/pages/SearchOrderPage.tsx
@@ -9,6 +9,7 @@ import {
 } from '@tanstack/react-table';
 import { Customer } from '../components/searchBar/CustomSearch';
 import { filter } from 'cypress/types/bluebird';
+import Navbar from '../components/NavbarComponent';
 
 type Order = { customerNumber: string; firstContact: string; id: string };
 
@@ -96,68 +97,67 @@ export default function SearchOrderPage() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center p-20 xl:px">
-      <h1 data-test="search-heading" className="text-5xl mb-11 min-w-max">
-        Se Ordrar
-      </h1>
+    <>
+      <Navbar title="Se Ordrar" />
+      <div className="flex min-h-screen flex-col items-center p-20 xl:px">
+        <input
+          placeholder="Sök order..."
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            filterOrders(e.target.value);
+          }}
+        />
 
-      <input
-        placeholder="Sök order..."
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          filterOrders(e.target.value);
-        }}
-      />
+        <div className="table-auto w-full rounded-lg p-10 bg-white shadow-md">
+          {/* <h2 className="text-xl font-bold text-gray-700 mb-3">Sök Ordrar</h2> */}
 
-      <div className="table-auto w-full rounded-lg p-10 bg-white shadow-md">
-        {/* <h2 className="text-xl font-bold text-gray-700 mb-3">Sök Ordrar</h2> */}
-
-        <table className="h-full w-full border-spacing-4 min-h-[20em] p-2">
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => {
-              return (
-                <tr key={headerGroup.id} className="text-sm">
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <th id={header.id} className="text-left pl-2">
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext(),
-                            )}
-                      </th>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </thead>
-          <tbody className="">
-            {table.getRowModel().rows.map((row) => {
-              return (
-                <tr
-                  key={row.id}
-                  className="shadow-lg hover:bg-blue-600 hover:font-sm hover:text-white"
-                >
-                  {row.getVisibleCells().map((cell) => {
-                    return (
-                      <td
-                        key={cell.id}
-                        className="text-[16px] mx-1 flex-wrap pl-2"
-                      >
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext(),
-                        )}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+          <table className="h-full w-full border-spacing-4 min-h-[20em] p-2">
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => {
+                return (
+                  <tr key={headerGroup.id} className="text-sm">
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <th id={header.id} className="text-left pl-2">
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext(),
+                              )}
+                        </th>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </thead>
+            <tbody className="">
+              {table.getRowModel().rows.map((row) => {
+                return (
+                  <tr
+                    key={row.id}
+                    className="shadow-lg hover:bg-blue-600 hover:font-sm hover:text-white"
+                  >
+                    {row.getVisibleCells().map((cell) => {
+                      return (
+                        <td
+                          key={cell.id}
+                          className="text-[16px] mx-1 flex-wrap pl-2"
+                        >
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext(),
+                          )}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
