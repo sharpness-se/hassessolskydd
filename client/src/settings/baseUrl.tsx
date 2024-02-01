6b6c9eded6ea0efb7673e7b245611b39efcdb1b5
Mikko Tirronen <mikkoatirronen@gmail.com>
2024-02-01 11:37:20 +0100

    fixing heroku deployment

--- client/src/settings/baseUrl.tsx
+++ client/src/settings/baseUrl.tsx
@@ -1,1 +1,1 @@
-export const baseUrl =process.env.NODE_ENV === "production" ? process.env.REACT_APP_BACKEND_URL : "http://localhost:8080"
+export const baseUrl = "https://hasses-be-c8520bea6cc2.herokuapp.com/"
