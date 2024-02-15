6ec507485e5de7f2c648b920ec2d8c06b127358c
Mikko Tirronen <mikkoatirronen@gmail.com>
2024-02-15 17:00:38 +0100

    fixed bug on radio button

--- client/src/pages/CreateOrderPage.tsx
+++ client/src/pages/CreateOrderPage.tsx
@@ -238,7 +238,7 @@ export default function CreateOrderPageComponent() {
                     className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
                     type="radio"
                     name="montering"
-                    value="avancerad"
+                    value="advanced"
                     checked={montering === 'advanced'}
                     onChange={(e) => {
                       setMontering(e.target.value);
