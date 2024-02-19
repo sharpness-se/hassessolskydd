3c0a484a27b515e223225ec46b5decea3ba7a448
arapg <hello@arpg.dev>
2024-02-19 15:23:00 +0100

    fixed Se Ordrar link

--- client/src/components/NavbarComponent.tsx
+++ client/src/components/NavbarComponent.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { FaAngleLeft, FaBars, FaX } from "react-icons/fa6";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaAngleLeft, FaBars, FaX } from 'react-icons/fa6';
 
 interface NavbarProps {
   title: string;
@@ -16,7 +16,7 @@ const Navbar: React.FC<NavbarProps> = ({ title }) => {
   return (
     <>
       <nav className="bg-blue-500 p-5 text-white sticky top-0 w-full flex items-center justify-between z-50">
-        <Link to={"/"}>
+        <Link to={'/'}>
           <div className="flex items-center">
             <FaAngleLeft className="align-middle" />
             <p className="font-bold">Tillbaka</p>
@@ -34,21 +34,21 @@ const Navbar: React.FC<NavbarProps> = ({ title }) => {
 
       <div
         className={`bg-blue-600 w-full lg:w-80 h-full fixed right-0 z-40 transition-transform duration-300 ${
-          isMenuOpen ? "translate-x-0" : "translate-x-full"
+          isMenuOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
         <ul className="text-white font-bold text-xl">
           <li className="p-5 shadow-sm">
-            <Link to={"/skapakund"}>Skapa ny kund</Link>
+            <Link to={'/skapakund'}>Skapa ny kund</Link>
           </li>
           <li className="p-5 shadow-sm">
-            <Link to={"/skapaorder"}>Skapa order</Link>
+            <Link to={'/skapaorder'}>Skapa order</Link>
           </li>
           <li className="p-5 shadow-sm">
-            <Link to={"/"}>Se kunder</Link>
+            <Link to={'/'}>Se kunder</Link>
           </li>
           <li className="p-5 shadow-sm">
-            <Link to={"/sökordrar"}>Se ordrar</Link>
+            <Link to={'/seordrar'}>Se ordrar</Link>
           </li>
         </ul>
       </div>
