285797260588faa82f673ebced5e0a20d34658ad
Mikko Tirronen <mikkoatirronen@gmail.com>
2024-04-11 18:37:46 +0200

    minor fix

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,14 +4,23 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-8jl4nya4dzypdch8.eu.auth0.com"
+      clientId="ZhDpd21sxSf0jK476c3F6GgqdsajQGav"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
